cfaa25d4475db7ad73d944f070e43df626945e83
BarbaraLorranne <83600368+BarbaraLorranne@users.noreply.github.com>
2023-01-11 22:08:15 -0300

    Add files via upload

---
+++ GameController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject introtxt;
+    public GameObject fimDeJogoPainel;
+    public int papelColetado;
+    void Start()
+    {
+        introtxt.gameObject.SetActive(true);
+        fimDeJogoPainel.gameObject.SetActive(false);
+        papelColetado = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fechaIntro();
+    }
+    public void fechaIntro() {
+        if (Input.GetMouseButtonDown(0))
+        {
+            introtxt.gameObject.SetActive(false);
+        }
+    }
+    public void FimDeJogo()
+    {
+        fimDeJogoPainel.gameObject.SetActive(true);
+    }
+    public void PegarPapel()
+    {
+        papelColetado ++;
+    }
+}
