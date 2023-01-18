32ee56083f36188373807666d8baf9a3e630d3d1
BarbaraLorranne <83600368+BarbaraLorranne@users.noreply.github.com>
2023-01-17 21:22:06 -0300

    Add files via upload
    NavMesh Agent to object moviment in AI.

---
+++ MariaScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MariaScript : MonoBehaviour
+{
+    private NavMeshAgent inimigo; //indica o objeto que irá se movimentar pelaarea azul da cena.
+    private Transform point; //lugar para onde o inimigo vai.
+
+    void Start()
+    {
+        inimigo = GetComponent<NavMeshAgent>();
+        point = GameObject.Find("PontoParada").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inimigo.SetDestination(point.position);
+    }
+}
