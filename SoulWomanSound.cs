68bfad8a8fa26c944f0a2552b3ba2b0b660a488c
BarbaraLorranne <83600368+BarbaraLorranne@users.noreply.github.com>
2023-01-18 21:57:30 -0300

    Add files via upload
    Soul Woman has a same mechanic of Maria.

---
+++ SoulWomanSound.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+[RequireComponent(typeof(AudioSource))]
+
+public class SoulWomanSound : MonoBehaviour
+{
+    public UnityEngine.AI.NavMeshAgent SoulWoman; //indica o objeto que irá se movimentar pelaarea azul da cena.
+    private Transform point; //lugar para onde o inimigo vai.
+
+    private AudioSource audioS;
+    private bool isAudio;
+
+    void Start()
+    {
+        SoulWoman = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        point = GameObject.Find("FPSController").transform;
+        isAudio = false;
+        audioS = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SoulWoman.SetDestination(point.position);
+
+        if(isAudio == true)
+        {
+            audioS.Play();
+        }
+        else
+        {
+            audioS.Stop();
+        }
+    }
+    public void OnTriggerStay(Collider coll)
+    {
+        if (coll.gameObject.tag == "Player"){
+            Debug.Log("IsCollider");
+            isAudio = true;
+        }   
+    }
+}
