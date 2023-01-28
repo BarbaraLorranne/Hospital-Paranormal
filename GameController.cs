cd86ab7e541a86955bd29793c01d852e1d132bb4
BarbaraLorranne <83600368+BarbaraLorranne@users.noreply.github.com>
2023-01-27 22:08:19 -0300

    Add files via upload
    This  script was updated. 
    This script go to controller speaks from player to FPS Francisco.

--- GameController.cs
+++ GameController.cs
@@ -7,32 +7,89 @@ using UnityEngine.SceneManagement;
 public class GameController : MonoBehaviour
 {
     public GameObject introtxt;
-    public GameObject fimDeJogoPainel;
-    public int papelColetado;
+    public GameObject gameOVerPanel;
+    public int paperCollected;
+    private GameObject PaperScript;
+    public GameObject franciscoTxt1;
+    public GameObject endDayOne;
+    public GameObject tiredPanel;
+    public int mission;
     void Start()
     {
         introtxt.gameObject.SetActive(true);
-        fimDeJogoPainel.gameObject.SetActive(false);
-        papelColetado = 0;
+        gameOVerPanel.gameObject.SetActive(false);
+        paperCollected = 0;
+        franciscoTxt1.SetActive(false);
+        endDayOne.SetActive(false);
+        tiredPanel.SetActive(false);
+        mission = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        fechaIntro();
+        CloseIntro();
+        CloseWarningSleep();
+        ClosePanelEndDayOne();
+        CollectedPaper();
+        missionPanel();
     }
-    public void fechaIntro() {
+    public void CloseIntro()
+    {
         if (Input.GetMouseButtonDown(0))
         {
             introtxt.gameObject.SetActive(false);
         }
     }
-    public void FimDeJogo()
+    public void CloseWarningSleep()
     {
-        fimDeJogoPainel.gameObject.SetActive(true);
+        if (Input.GetMouseButtonDown(0))
+        {
+            tiredPanel.gameObject.SetActive(false);
+        }
     }
-    public void PegarPapel()
+    public void ClosePanelEndDayOne()
     {
-        papelColetado ++;
+        if (Input.GetMouseButtonDown(0))
+        {
+            endDayOne.gameObject.SetActive(false);
+        }
+    }
+
+    public void EndGame()
+    {
+        gameOVerPanel.gameObject.SetActive(true);
+    }
+    public void CatchPaper()
+    {
+        paperCollected++;
+    }
+    public void CollectedPaper()
+    {
+        if (paperCollected == 3)
+        {
+            //por som do capeta;
+            franciscoTxt1.SetActive(true);
+            StartCoroutine(FrancisTxt1Destroy());
+            paperCollected++;
+        }
+    }
+    public void missionPanel()
+    {
+        if (franciscoTxt1 == false)
+        {
+            tiredPanel.SetActive(true);
+        }
+        if(tiredPanel == false)
+        {
+            endDayOne.SetActive(true);
+            //faz um teleporte do Francisco para o próprio quarto.
+        }
+    }
+    IEnumerator FrancisTxt1Destroy()
+    {
+        yield return new WaitForSeconds(3f);
+        Destroy(franciscoTxt1);
+
     }
 }
