4f1c8ed895d96066b4d41f656e162fc1177bc488
BarbaraLorranne <83600368+BarbaraLorranne@users.noreply.github.com>
2023-02-07 21:31:30 -0300

    Add files via upload
    This enemy its a soul woman and her has a component "Nav Mesh" with de IA to moving her about map.

---
+++ SoulWomanSound.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+[RequireComponent(typeof(AudioSource))]
+
+public class SoulWomanSound : MonoBehaviour
+{
+    public UnityEngine.AI.NavMeshAgent SoulWoman; //indica o objeto que irá se movimentar pela area azul da cena.
+    private Transform point; //lugar para onde o inimigo vai.
+
+    private AudioSource audioS;
+    private bool isAudio;
+
+    public GameObject JumpScarePanel;
+    void Start()
+    {
+        SoulWoman = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        point = GameObject.Find("FPSController").transform;
+        isAudio = false;
+        audioS = GetComponent<AudioSource>();
+        JumpScarePanel.SetActive(false);
+        StopCoroutine(GoToGO());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SoulWoman.SetDestination(point.position);
+
+        if(isAudio == true)
+        {
+            audioS.Play();
+        }
+        else
+        {
+            audioS.Stop();
+        }
+    }
+    public void OnTriggerStay(Collider coll)
+    {
+        if (coll.gameObject.tag == "Player"){
+            Debug.Log("IsCollider");
+            isAudio = true;
+            JumpScarePanel.SetActive(true);
+            Time.timeScale = 0;
+            StartCoroutine(GoToGO());
+        }
+        else
+        {
+            JumpScarePanel.SetActive(false);
+            isAudio = false;
+            Time.timeScale = 1;
+            StopCoroutine(GoToGO());
+        }
+    }
+    IEnumerator GoToGO()
+    {
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene("GameOver");
+    }
+}
