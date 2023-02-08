1b4dd27e5fc433f6fcc20590735b15aa7ade2c45
BarbaraLorranne <83600368+BarbaraLorranne@users.noreply.github.com>
2023-02-07 21:33:53 -0300

    Add files via upload
    This script commands Game Over scene in this game.

---
+++ CanvasGameOver.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CanvasGameOver : MonoBehaviour
+{
+    private GameObject CanvasGO;
+    void Start()
+    {
+        CanvasGO = GameObject.Find("CanvasGO");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GoToLvl1();
+    }
+    public void GoToLvl1()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene("Fase1");
+        }
+    }
+}
